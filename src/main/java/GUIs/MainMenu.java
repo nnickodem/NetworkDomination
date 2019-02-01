490824811be60627b6faea7e5283c9e45e5a5113
Matt <mfruechte.97@gmail.com>
2019-02-01 11:42:56 -0600

    Main Menu Changes
    Resolves #2

---
+++ src/main/java/GUIs/MainMenu.java
@@ -0,0 +1,54 @@
+package GUIs;
+
+import javax.swing.*;
+import java.awt.*;
+import static java.lang.System.exit;
+
+public class MainMenu extends JFrame {
+    private JButton playButton = new JButton("Play");
+    private JButton loadButton = new JButton("Load Game");
+    private JButton optionsButton = new JButton("Settings");
+    private JButton exitButton = new JButton("Exit");
+
+    private int buttonHeight = 40;
+    private int buttonWidth = 150;
+
+    //Add each button to the JPanel
+    public MainMenu(){
+        JFrame gameWindow = new JFrame();
+        JPanel mainMenu = new JPanel(new GridBagLayout());
+        mainMenu.setBackground(Color.DARK_GRAY);
+        GridBagConstraints constraints = new GridBagConstraints();
+        //mainMenu.setBounds(0,0,800,600);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(3,0,3,0);
+        constraints.fill = GridBagConstraints.CENTER;
+        playButton.setPreferredSize(new Dimension(buttonWidth,buttonHeight));
+        mainMenu.add(playButton, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.insets = new Insets(3,0,3,0);
+        loadButton.setPreferredSize(new Dimension(buttonWidth,buttonHeight));
+        mainMenu.add(loadButton, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.insets = new Insets(3,0,3,0);
+        optionsButton.setPreferredSize(new Dimension(buttonWidth,buttonHeight));
+        mainMenu.add(optionsButton, constraints);
+
+        exitButton.addActionListener(e-> exit(0));
+        constraints.gridx = 0;
+        constraints.gridy = 3;
+        constraints.insets = new Insets(3,0,3,0);
+        exitButton.setPreferredSize(new Dimension(buttonWidth,buttonHeight));
+        mainMenu.add(exitButton, constraints);
+
+        //Add the mainMenu panel to the frame and set the window size
+        add(mainMenu);
+        setSize(800,600);
+    }
+}
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,20 @@
+import GUIs.MainMenu;
+import GUIs.Test;
+
+import javax.swing.*;
+
 /**
  * Temporary
  */
 public class main {
     public static void main(String args[]) {
-        
+        // set look and feel to the system and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception c){
+            c.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> new MainMenu().setVisible(true));
     }
 }
