c6217c4c101f7dec54ccc596bb64dd33572b448f
Nathan Nickodem <nnickodem@live.com>
2019-02-05 11:00:42 -0600

    Updated game object classes, added GameHandler class to handle game ticks and call for updates
    Resolves #14

---
+++ src/main/java/Objects/Packets/Botnet.java
@@ -0,0 +1,11 @@
+package Objects.Packets;
+
+public class Botnet extends Packet {
+
+    public Botnet(final int team) {
+        //super(team);
+        setCost(1);
+        setStealth(0);
+    }
+
+}
