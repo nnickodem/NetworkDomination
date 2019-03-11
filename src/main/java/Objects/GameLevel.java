cb5cf51484f0443755fd0098139c2867f6a0b7e8
1bucko2 <34140898+1bucko2@users.noreply.github.com>
2019-03-11 12:07:01 -0500

    Added level description box (#73)
    * Added level description box
    
    * Added multiple line functionalities
    
    * Update GameLevel.java

--- src/main/java/Objects/GameLevel.java
+++ src/main/java/Objects/GameLevel.java
@@ -13,13 +13,20 @@ public class GameLevel {
 	private String[][] levelMap;
 	private List<Map.Entry<String,String>> connections;
 	private Map<String, NetworkDevice> idToDeviceObject;
+	private String description;
+	private List<String> primaryObjectives;
+	private List<String> secondaryObjectives;
 
 	public GameLevel(final String [][] levelMap,
 					 final List<Map.Entry<String, String>> connections,
-					 final Map<String, NetworkDevice> idToDeviceObject) {
+					 final Map<String, NetworkDevice> idToDeviceObject, String description,
+					 List<String> primaryObjectives, List<String> secondaryObjectives) {
 		this.levelMap = levelMap;
 		this.connections = connections;
 		this.idToDeviceObject = idToDeviceObject;
+		this.description = description;
+		this.primaryObjectives = primaryObjectives;
+		this.secondaryObjectives = secondaryObjectives;
 	}
 
 	public String[][] getLevelMap() {
@@ -33,4 +40,16 @@ public class GameLevel {
 	public Map<String, NetworkDevice> getIdToDeviceObject() {
 		return idToDeviceObject;
 	}
+
+	public String getDescription() {
+    		return description; 
+  	}
+
+	public List<String> getPrimaryObjectives() {
+    		return primaryObjectives; 
+  	}
+
+	public List<String> getSecondaryObjectives() {
+    		return secondaryObjectives; 
+  	}
 }
